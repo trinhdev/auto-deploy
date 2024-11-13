c9cb5095f310350475876192b40132c444b2006c
trinhdev <trinhhuynhdp@gmail.com>
2024-11-14 02:54:15 +0700

    12#

--- public/deploy.php
+++ public/deploy.php
@@ -7,7 +7,6 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 $payload = file_get_contents('php://input');
 if ('sha1=' . hash_hmac('sha1', $payload, $secret) === $signature) {
     $output = shell_exec('cd ../ && git pull origin main');
-    $output .= shell_exec('ls');
     $output .= shell_exec('composer install --no-interaction --prefer-dist --optimize-autoloader');
     $output .= shell_exec('php artisan migrate --force');
     $output .= shell_exec('php artisan config:cache');
