f07b3bc74eec8f970bdd1e50bd7a4bb4930519cc
trinhdev <trinhhuynhdp@gmail.com>
2024-11-14 02:47:05 +0700

    12#

--- public/deploy.php
+++ public/deploy.php
@@ -6,8 +6,8 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 // Xác thực chữ ký
 $payload = file_get_contents('php://input');
 if ('sha1=' . hash_hmac('sha1', $payload, $secret) === $signature) {
-    $output = shell_exec('cd .. && git pull origin main');
-    $output = shell_exec('ls');
+    $output = shell_exec('cd ../ && git pull origin main');
+    $output .= shell_exec('ls');
     $output .= shell_exec('composer install --no-interaction --prefer-dist --optimize-autoloader');
     $output .= shell_exec('php artisan migrate --force');
     $output .= shell_exec('php artisan config:cache');
